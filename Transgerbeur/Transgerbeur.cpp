d577f4c9552874507c663efbebe38d6ffca2e5ba
axel Fourreau <fourreauaxel@gmail.com>
2024-11-28 12:00:33 +0100

    commit 3 (moyen compris)

--- Transgerbeur/Transgerbeur.cpp
+++ Transgerbeur/Transgerbeur.cpp
@@ -32,6 +32,20 @@ int main()
 {
 	cout << "Bienvenue dans le Transgerbeur";
 	SCasier tabCasier[9];
-	
+	SRouleau  ro1;
+	SPalette pa1;
+	pa1.poids = 40;
+	strcpy_s(pa1.reference, "Attila veut mortis"); // ignisalisation des chaine de caractere
+	EEtat etatC1;
+	etatC1 = vide;
+	cont1.pal = pa1;
+	cont1.roul = ro1;
+	SCasier c1;
+	SCasier c2;
+	c1.cont = cont1;
+	c1.etat = etatC1;
+	c1.px = 2;
+	c1.py = 3;
+	c2.cont = cont1;
 
 }
